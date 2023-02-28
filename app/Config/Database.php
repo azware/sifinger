3614422695f6e192f2741c721a42e5e55926115d
Azzam PC <anh.blackoni@gmail.com>
2023-02-28 13:21:30 +0700

    Create Mesin Finger Pages

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'username' => 'root',
+        'password' => 'azsdmrsa1991',
+        'database' => 'ftm', // DB Fingerprint
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
